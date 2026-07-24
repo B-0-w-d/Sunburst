021f03c98a057dba83510fdbbc6a0e719ee1f228
Phan Hoang Trong Tin <Chouphan1207@gmail.com>
2026-07-24 10:57:50 +0700

    fix tương thích version và readme.

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
         laravel({
             input: [
                 'resources/css/app.css',
-                'resources/css/home.css',
+                'resources/css/pages/home.css',
                 'resources/js/app.js'
             ],
             refresh: true,
